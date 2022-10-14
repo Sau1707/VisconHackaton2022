64345ca95a80f5612e314952d3817d5ab1586b38
Anja Sjöström <anjasjostrom@student-net-hg-3283.intern.ethz.ch>
2022-10-15 00:19:28 +0200

    basic card template created

--- Frontend/components/box.jsx
+++ Frontend/components/box.jsx
@@ -5,16 +5,15 @@ import Card from 'react-bootstrap/Card';
 
 export default function Box(props) {
     return (
-        <Card style={{ width: '18rem' }}>
-          <Card.Img variant="top" src="holder.js/100px180" />
-          <Card.Body>
-            <Card.Title>Card Title</Card.Title>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </Card.Text>
-            <Button variant="primary">Go somewhere</Button>
-          </Card.Body>
-        </Card>
+        <Card style={{ width: '50rem' }}>
+            <Card.Body>
+              <Card.Title>{props.title}</Card.Title>
+              <Card.Text>
+                This is a longer card with supporting text below as a natural
+                lead-in to additional content. This content is a little bit
+                longer.
+              </Card.Text>
+            </Card.Body>
+          </Card>
       );
 }

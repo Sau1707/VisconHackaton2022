8686187e75f79de247e939491b4c81ef9b79e0a8
Adriano Hernandez <adrianoh@mit.edul>
2022-10-15 00:30:13 +0200

    backend commenting/design

---
+++ Backend/weekly_update.py
@@ -0,0 +1,15 @@
+class StoryCreator:
+    # XXX This needs to be done, it should be able to create a comma-delimited story
+    # XXX using requests from ASVZ for the current activities that are available. We
+    # XXX can start by picking arbitrary activities.
+    pass
+
+class WeeklyUpdateExecutor:
+    # XXX This should basically have one function that goes through every active user
+    # XXX and then pairs them out (if odd number then you can ignore one arbitrary
+    # XXX guy) and then for each pair updates them (in Crud function `handle_post`)
+    # XXX such that they are eachothers opponents and they each get the same story.
+    # XXX Stories can be random otherwise (i.e. across different pairs).
+    # XXX Make sure to not forget to update little things like the week etc...
+    # XXX Make sure to update ALL
+    pass

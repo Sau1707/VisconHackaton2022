0127aa0d1f3fc4a53e678d9b8fa39bc39b986a7d
Anja Sjöström <anjasjostrom@student-net-hg-3283.intern.ethz.ch>
2022-10-15 13:19:14 +0200

    merge

---
+++ Frontend/components/Tracker.jsx
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';  
+import { Bluetooth } from 'react-bootstrap-icons';
+import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
+
+class Tracker extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            percentage: 60
+        };
+    }
+
+    render() {
+        var myStyle = {
+            marginLeft: 200,
+            width: 200,
+            height: 200
+        }
+        return (
+            <div style = {myStyle}>
+                <CircularProgressbar value={this.state.percentage} text={`${this.state.percentage}%` } />
+            </div>
+        )
+    }
+
+}
+
+export default Tracker;
--- Frontend/pages/achievements.js
+++ Frontend/pages/achievements.js
@@ -1,13 +1,17 @@
 import ChartBox from "../components/ChartBox"
 import Grid from "../components/grid"
 import Box from "../components/box"
+import Tracker from "../components/Tracker"
 
 export default function Achievements() {
     return (
         <>
             <h1 style = {{textAlign: "center", marginTop: "60px"}}>My Achievements</h1>
-            <div style = {{marginLeft: "100px", marginTop: "200px", width: "fit-content"}}>
+            <div style = {{marginLeft: "100px", marginTop: "200px", width: "fit-content", display: "flex", flexDirection: "row"}}>
                 <ChartBox></ChartBox>
+                <Tracker></Tracker>
+                <Tracker></Tracker>
+                <Tracker></Tracker>
             </div>    
         </>
     )

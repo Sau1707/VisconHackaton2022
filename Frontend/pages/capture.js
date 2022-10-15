b13454d135e2cb1bc9dbd968a12042464ce0ad94
Furkan Özkan <h.furkan.oezkan@gmail.com>
2022-10-15 15:50:40 +0200

    10 additional flags

--- Frontend/pages/capture.js
+++ Frontend/pages/capture.js
@@ -9,6 +9,16 @@ export default function Capture() {
             <Flag img="/sports/beachwolley.png" />
             <Flag img="/sports/bigair.png" />
             <Flag img="/sports/arrow.png" />
+            <Flag img="/sports/dance.png" />
+            <Flag img="/sports/box.png" />
+            <Flag img="/sports/taekwondo.png" />
+            <Flag img="/sports/football.png" />
+            <Flag img="/sports/swimming.png" />
+            <Flag img="/sports/diving.png" />
+            <Flag img="/sports/tabletennis.png" />
+            <Flag img="/sports/hiking.png" />
+            <Flag img="/sports/climbing.png" />
+            <Flag img="/sports/fencing.png" />
         </FlagBox>
     )
 }

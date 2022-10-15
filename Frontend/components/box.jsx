f745db5820c06c59cdb628d6f88ce02d8b8bc94d
Sau1707 <83017316+Sau1707@users.noreply.github.com>
2022-10-15 04:05:19 +0200

    Finisch home + login

--- Frontend/components/box.jsx
+++ Frontend/components/box.jsx
@@ -1,19 +1,34 @@
-import style from "./example.module.css"
-import Button from 'react-bootstrap/Button';
+import style from "./Box.module.css"
 import Card from 'react-bootstrap/Card';
 import { useRouter } from 'next/router'
 
-
 export default function Box(props) {
+    const router = useRouter()
+
+    const redirect = (e) => {
+        e.preventDefault()
+        props.href ? router.push(props.href) : null
+    }
 
     return (
-        <Card>
-            <Card.Body>
-                <Card.Title>{props.title}</Card.Title>
-                <Card.Text>
-                    {props.children}
-                </Card.Text>
-            </Card.Body>
-        </Card>
+        <div className={style.flipCard} onClick={redirect}>
+            <div className={style.flipCardInner}>
+                <div className={style.flipCardFront}>
+                    <h1 style={{ padding: 100 }}> {props.title} </h1>
+                </div>
+                <div className={style.flipCardBack}>
+                    <Card className={style.cardBox}>
+                        <Card.Body>
+                            <Card.Title> {props.title} </Card.Title>
+                            <Card.Text>
+                                {props.children}
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                </div>
+            </div>
+        </div>
+
     );
+
 }
--- Frontend/components/example.jsx
+++
@@ -1,5 +0,0 @@
-import style from "./example.module.css"
-
-export default function Example(props) {
-    return (<h1 className={style.exampleClass}> {props.children} </h1>)
-}

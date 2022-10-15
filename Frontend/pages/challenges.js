47941ec58cf3dbccaa8e14434a63077da55d55d7
Sau1707 <83017316+Sau1707@users.noreply.github.com>
2022-10-15 11:20:31 +0200

    Update

--- Frontend/pages/challenges.js
+++ Frontend/pages/challenges.js
@@ -3,36 +3,10 @@ import axios from 'axios'
 import { useState, useEffect } from "react";
 
 export default function Challenges() {
-<<<<<<< Updated upstream
-    const [getMessage, setGetMessage] = useState({})
-
-    // the flask server is exposed on port 5000
-    useEffect(()=>{
-      axios.get('http://localhost:5000/flask/hello').then(response => {
-        console.log("SUCCESS", response)
-        setGetMessage(response)
-      }).catch(error => {
-        console.log(error)
-      })
-  
-    }, [])
-    return (
-        <header className="App-header">
-         
-          <p>React + Flask Tutorial</p>
-          <div>{getMessage.status === 200 ? 
-            <h3>{getMessage.data.message}</h3>
-            :
-            <h3>LOADING</h3>}</div>
-        </header>
-    );
-=======
-
     /* Fetch data here, */
     return (
         <>
             <ChallengeBox user={80} competitor={20} />
         </>
     )
->>>>>>> Stashed changes
 }

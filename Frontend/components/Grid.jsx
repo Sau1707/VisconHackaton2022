f34de61f46b405f08fada50104b66d6dc85e48a4
Sau1707 <83017316+Sau1707@users.noreply.github.com>
2022-10-16 11:48:46 +0200

    Add

---
+++ Frontend/components/Grid.jsx
@@ -0,0 +1,20 @@
+import style from "./grid.module.css"
+import Container from 'react-bootstrap/Container';
+import { Row, Col } from "react-bootstrap";
+
+export default function Grid(props) {
+    console.log()
+    return (
+        <Container className={style.mainBox} style = {{...props.style}}>
+            <Row xs={1} md={2}>
+                {props.children.map((e, i) => (
+                    <Col
+                        key={i}
+                        className={style.fake}>
+                        {e}
+                    </Col>
+                ))}
+            </Row>
+        </Container>
+    );
+}

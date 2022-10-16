aafc0bbbdd9a74860fc2164b4c92654303c00c06
Sau1707 <83017316+Sau1707@users.noreply.github.com>
2022-10-16 11:51:01 +0200

    Update Grid.jsx

--- Frontend/components/Grid.jsx
+++ Frontend/components/Grid.jsx
@@ -1,11 +1,11 @@
-import style from "./grid.module.css"
+import style from "./Grid.module.css"
 import Container from 'react-bootstrap/Container';
 import { Row, Col } from "react-bootstrap";
 
 export default function Grid(props) {
     console.log()
     return (
-        <Container className={style.mainBox} style = {{...props.style}}>
+        <Container className={style.mainBox} style={{ ...props.style }}>
             <Row xs={1} md={2}>
                 {props.children.map((e, i) => (
                     <Col

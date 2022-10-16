dcabd83952ab1dc1c26a2236fe8251ffbd1a1fda
Adriano Hernandez <adrianoh@mit.edul>
2022-10-16 08:45:23 +0200

    WIP

--- Backend/app.py
+++ Backend/app.py
@@ -1,37 +1,58 @@
 from flask import Flask, request
-import json
-from handler import request_handler, weekly_update_handler
+import logging
+from typing import Callable, Any
 app = Flask(__name__)
 
-@app.route('/', methods=["GET", "POST"])
-def handle():
-    #return {"Exists": "true"}
-    # https://stackoverflow.com/questions/38637249/why-is-my-flask-request-object-always-empty
-    # Form goes into
-    #   request.form
-    # Query Arguments (in the URL)
-    #  request.args
-
-    # print(request.form) # DEBUG
-    # print(request.args) # DEBUG
+def handler(request: Any, handle_func: Callable[[Any], Any]):
     rq = {}
     try:
         rq['Json'] = request.json if request.is_json else {}
     except Exception as e:
-        print(f"fml: {e}")
+        logging.warning(f'Failed to get request.json: {e}')
         rq['Json'] = {}
+    try:
+        rq['Data'] = request.data
+    except Exception as e:
+        logging.warning(f'Failed to get request.data: {e}')
+        rq['Data'] = b''
     rq['Args'] = request.args
     rq['Method'] = request.method
-    return request_handler(rq)
+    return handle_func(rq)
 
+# Getters (All information should be in the Args section)
+@app.route('/user_history', methods=["GET"])
+def handle_user_history():
+    return handler(request, lambda x : x) # XXX
+
+@app.route('/user_details', methods=["GET"])
+def handle_user_details():
+    return handler(request, lambda x : x) # XXX
+
+@app.route('/user_details', methods=["GET"])
+def handle_user_existence() -> bool:
+    return handler(request, lambda x : x) # XXX
+
+# Creators
+@app.route('/user_details', methods=["POST"])
+def handle_create_user():
+    return handler(request, lambda x : x) # XXX
+
+# Updators
+@app.route('/user_details', methods=["POST"])
+def handle_user_weekly_progress():
+    return handler(request, lambda x : x) # XXX
+@app.route('/user_details', methods=["POST"])
+def handle_user_preferences():
+    return handler(request, lambda x : x) # XXX
+@app.route('/user_details', methods=["POST"])
+def handle_user_history():
+    return handler(request, lambda x : x) # XXX
+
+
+# Admin Route
 @app.route('/weekly_update/', methods=["POST"])
 def handle_weekly_update():
-    # This is a special handler
-    rq = {}
-    rq['Form'] = request.form
-    rq['Args'] = request.args
-    rq['Method'] = request.method
-    return weekly_update_handler(rq)
+    return handler(request, lambda x : x) # XXX
 
 # Run a flask app on debug mode
 if __name__=='__main__':

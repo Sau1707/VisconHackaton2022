f8d4968efeaa845d830f4df5a4ff6d47e1983ffc
Sau1707 <83017316+Sau1707@users.noreply.github.com>
2022-10-16 12:09:05 +0200

    Fix descriptions

--- Frontend/pages/index.js
+++ Frontend/pages/index.js
@@ -5,7 +5,6 @@ import { useSession, signOut } from "next-auth/react";
 import { getUserExists, addNewUser } from "../fetch/util"
 import { useRouter } from 'next/router'
 import Avatar from "../components/Avatar"
-import { Translate } from "react-bootstrap-icons";
 
 const ChallengesText = "Level up your fitness game by competing with others for flags and... maybe... legendary trophies. Every week a new challenge is issued and players are paired by skill level, commitment, and interests. The first to complete the tasks wins that weeks' flag. With enough flags, you will find greater rewards to encourage you forward into a way of lifelong health and vigor."
 const TeamText = "Coming soon."
@@ -20,68 +19,68 @@ export default function Home() {
     const { data: session, status } = useSession();
 
     /* Check if user exist, if dont -> resent to ask for preferences */
-    getUserExists(testUsername).then(e => {
-        if (!e.data.Exists) router.push("/preferences")
-        else console.log("User already logged")
-    })
+    // getUserExists(testUsername).then(e => {
+    //     if (!e.data.Exists) router.push("/preferences")
+    //     else console.log("User already logged")
+    // })
 
     return (
         <>
             <h1 style={{ textAlign: "center", marginTop: "5vh", background: "none" }}> Welcome {session.user.name} </h1>
             <Avatar />
             <div>
-               <div style = {{border: "0px solid green", position: "relative", height: "400px"}}> 
-             <Trophy />
-            </div>
-            <div>
-            <Grid>
-                <Box
-                    href="challenges"
-                    image="/dalle/challenge.png"
-                    front={<></>}
-                    back={
-                        <div style={{ padding: 30 }}>
-                            <h2 className="with-eight" style={{ fontWeight: "bold" }}> Challenges </h2>
-                            <h5 className="with-eight" > {ChallengesText} </h5>
-                        </div>
-                    }
-                />
-                <Box
-                    href=""
-                    title="Team"
-                    image="/dalle/team.png"
-                    front={<></>}
-                    back={
-                        <div style={{ padding: 30 }}>
-                            <h2 className="with-eight" style={{ fontWeight: "bold" }}> Team </h2>
-                            <h5 className="with-eight"> {TeamText} </h5>
-                        </div>
-                    }
-                />
-                <Box
-                    href="capture"
-                    image="/dalle/cups.png"
-                    front={<></>}
-                    back={
-                        <div style={{ padding: 30 }}>
-                            <h2 className="with-eight" style={{ fontWeight: "bold" }}> Capture The Flag  </h2>
-                            <h5 className="with-eight"> {CaptureTheFlagText} </h5>
-                        </div>
-                    }
-                />
-                <Box
-                    href="achievements"
-                    image="/dalle/plot.png"
-                    front={<></>}
-                    back={
-                        <div style={{ padding: 30 }}>
-                            <h2 className="with-eight" style={{ fontWeight: "bold" }}> My Achievements </h2>
-                            <h5 className="with-eight" > {AchievementsText} </h5>
-                        </div>
-                    }
-                />
-            </Grid>
-            </div>
+                <div style={{ border: "0px solid green", position: "relative", height: "400px" }}>
+                    <Trophy />
+                </div>
+                <div>
+                    <Grid>
+                        <Box
+                            href="challenges"
+                            image="/dalle/challenge.png"
+                            front={<></>}
+                            back={
+                                <div style={{ padding: 30 }}>
+                                    <h2 className="with-eight" style={{ fontWeight: "bold" }}> Challenges </h2>
+                                    <h5 className="with-eight" > {ChallengesText} </h5>
+                                </div>
+                            }
+                        />
+                        <Box
+                            href=""
+                            title="Team"
+                            image="/dalle/team.png"
+                            front={<></>}
+                            back={
+                                <div style={{ padding: 30 }}>
+                                    <h2 className="with-eight" style={{ fontWeight: "bold" }}> Team </h2>
+                                    <h5 className="with-eight"> {TeamText} </h5>
+                                </div>
+                            }
+                        />
+                        <Box
+                            href="capture"
+                            image="/dalle/cups.png"
+                            front={<></>}
+                            back={
+                                <div style={{ padding: 30 }}>
+                                    <h2 className="with-eight" style={{ fontWeight: "bold" }}> Capture The Flag  </h2>
+                                    <h5 className="with-eight"> {CaptureTheFlagText} </h5>
+                                </div>
+                            }
+                        />
+                        <Box
+                            href="achievements"
+                            image="/dalle/plot.png"
+                            front={<></>}
+                            back={
+                                <div style={{ padding: 30 }}>
+                                    <h2 className="with-eight" style={{ fontWeight: "bold" }}> My Achievements </h2>
+                                    <h5 className="with-eight" > {AchievementsText} </h5>
+                                </div>
+                            }
+                        />
+                    </Grid>
+                </div>
             </div>
         </>
     )
